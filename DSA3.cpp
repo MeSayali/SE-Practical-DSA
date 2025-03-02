25161e3a80d04ae813dff2e00f7f0b3a65a1ea5f
Sayali Pawar <139886338+MeSayali@users.noreply.github.com>
2025-03-03 00:41:13 +0530

    Update DSA3.cpp

--- DSA3.cpp
+++ DSA3.cpp
@@ -5,12 +5,12 @@ class Node {
 public:
     int info;
     Node *left, *right;
-    bool lthread, rthread;
+    bool lthread, rthread; // Thread indicators for left and right
     
     Node(int key) {
         info = key;
         left = right = NULL;
-        lthread = rthread = true;//not pointting to children but can be used as thread
+        lthread = rthread = true; // Initially, threads are true
     }
 };
 
@@ -18,20 +18,22 @@ class TBTree {
 private:
     Node *root;
 
+    // Function to find the inorder successor of a given node
     Node* inorderSuccessor(Node* ptr) {
-        if (ptr->rthread)////if it is thread==true
-            return ptr->right;//it will return it
-        ptr = ptr->right;//if not move to right child
-        while (!ptr->lthread)//if ptr is not lthread move to leftmost node
+        if (ptr->rthread) // If right thread exists, return right pointer
+            return ptr->right;
+        ptr = ptr->right;
+        while (!ptr->lthread) // Move to the leftmost node
             ptr = ptr->left;
         return ptr;
     }
 
+    // Function to find the inorder predecessor of a given node
     Node* inorderPredecessor(Node* ptr) {
-        if (ptr->lthread)
+        if (ptr->lthread) // If left thread exists, return left pointer
             return ptr->left;
         ptr = ptr->left;
-        while (!ptr->rthread)
+        while (!ptr->rthread) // Move to the rightmost node
             ptr = ptr->right;
         return ptr;
     }
@@ -47,33 +49,33 @@ public:
 
 void TBTree::insert(int ikey) {
     Node *ptr = root, *par = NULL;
-    while (ptr != NULL) {
-        if (ikey == ptr->info) {
+    while (ptr != NULL) { // Traverse to find the correct position
+        if (ikey == ptr->info) { // Duplicate key not allowed
             cout << "Duplicate Key\n";
             return;
         }
         par = ptr;
-        if (ikey < ptr->info) {
+        if (ikey < ptr->info) { // Move left if smaller
             if (!ptr->lthread)
                 ptr = ptr->left;
             else
                 break;
-        } else {
+        } else { // Move right if greater
             if (!ptr->rthread)
                 ptr = ptr->right;
             else
                 break;
         }
     }
-    Node* tmp = new Node(ikey);
+    Node* tmp = new Node(ikey); // Create new node
     if (par == NULL) {
-        root = tmp;
-    } else if (ikey < par->info) {
+        root = tmp; // First node in tree
+    } else if (ikey < par->info) { // Attach new node to left
         tmp->left = par->left;
         tmp->right = par;
         par->lthread = false;
         par->left = tmp;
-    } else {
+    } else { // Attach new node to right
         tmp->left = par;
         tmp->right = par->right;
         par->rthread = false;
@@ -87,10 +89,10 @@ void TBTree::inorder() {
         return;
     }
     Node* ptr = root;
-    while (!ptr->lthread)
+    while (!ptr->lthread) // Move to the leftmost node
         ptr = ptr->left;
     
-    while (ptr != NULL) {
+    while (ptr != NULL) { // Traverse inorder
         cout << ptr->info << " ";
         ptr = inorderSuccessor(ptr);
     }
@@ -105,11 +107,11 @@ void TBTree::preorder() {
     Node* ptr = root;
     while (ptr != NULL) {
         cout << ptr->info << " ";
-        if (!ptr->lthread)
+        if (!ptr->lthread) // Move to left child if exists
             ptr = ptr->left;
-        else if (!ptr->rthread)
+        else if (!ptr->rthread) // Move to right child if exists
             ptr = ptr->right;
-        else {
+        else { // Move to next threaded node
             while (ptr != NULL && ptr->rthread)
                 ptr = ptr->right;
             if (ptr != NULL)
@@ -121,16 +123,16 @@ void TBTree::preorder() {
 
 void TBTree::deleteNode(int key) {
     Node *ptr = root, *par = NULL;
-    while (ptr != NULL) {
+    while (ptr != NULL) { // Find node to delete
         if (key == ptr->info)
             break;
         par = ptr;
-        if (key < ptr->info) {
+        if (key < ptr->info) { // Move left if key is smaller
             if (!ptr->lthread)
                 ptr = ptr->left;
             else
                 return;
-        } else {
+        } else { // Move right if key is greater
             if (!ptr->rthread)
                 ptr = ptr->right;
             else
@@ -142,31 +144,31 @@ void TBTree::deleteNode(int key) {
         return;
     }
     
-    if (!ptr->lthread && !ptr->rthread) {
-        Node* succ = inorderSuccessor(ptr);
-        ptr->info = succ->info;
-        deleteNode(succ->info);
+    if (!ptr->lthread && !ptr->rthread) { // Node has two children
+        Node* succ = inorderSuccessor(ptr); // Find inorder successor
+        ptr->info = succ->info; // Replace value
+        deleteNode(succ->info); // Recursively delete successor
         return;
     }
     
-    Node *child = (ptr->lthread) ? ptr->right : ptr->left;
+    Node *child = (ptr->lthread) ? ptr->right : ptr->left; // Find child
     if (par == NULL) {
-        root = child;
-    } else if (ptr == par->left) {
+        root = child; // If deleting root node
+    } else if (ptr == par->left) { // If node is left child
         par->left = child;
         par->lthread = ptr->lthread;
-    } else {
+    } else { // If node is right child
         par->right = child;
         par->rthread = ptr->rthread;
     }
-    if (!ptr->lthread) {
+    if (!ptr->lthread) { // Update predecessor thread
         Node* pred = inorderPredecessor(ptr);
         pred->right = ptr->right;
-    } else if (!ptr->rthread) {
+    } else if (!ptr->rthread) { // Update successor thread
         Node* succ = inorderSuccessor(ptr);
         succ->left = ptr->left;
     }
-    delete ptr;
+    delete ptr; // Delete the node
 }
 
 int main() {
